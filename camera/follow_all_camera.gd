0c8c6300077fa94fe7a4a6d22c71c0958a89cf57
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2026-01-31 00:06:55 +0100

    Implement FollowAllCamera

---
+++ camera/follow_all_camera.gd
@@ -0,0 +1,29 @@
+class_name FollowAllCamera
+extends Camera2D
+
+
+const SCREEN_MARGIN: int = 256
+
+
+func _process(_delta: float) -> void:
+	self.global_position = Vector2.ZERO
+	var min_pos: Vector2 = Vector2.INF
+	var max_pos: Vector2 = -Vector2.INF
+	
+	for player: PlayerData in Game.players:
+		self.global_position += player.ship.global_position / Game.players.size()
+		min_pos.x = min(min_pos.x, player.ship.global_position.x)
+		min_pos.y = min(min_pos.y, player.ship.global_position.y)
+		max_pos.x = max(max_pos.x, player.ship.global_position.x)
+		max_pos.y = max(max_pos.y, player.ship.global_position.y)
+		
+	var current_viewport_res: Vector2 = get_viewport().get_visible_rect().size
+	var target_screen_size: Vector2 = Vector2(
+		max_pos.x - min_pos.x + SCREEN_MARGIN * 2,
+		max_pos.y - min_pos.y + SCREEN_MARGIN * 2
+	)
+	
+	var zoom_vector: Vector2 = current_viewport_res / target_screen_size
+	var zoom_value: float = min(zoom_vector.x, zoom_vector.y)
+	
+	self.set_zoom(Vector2(zoom_value, zoom_value))
