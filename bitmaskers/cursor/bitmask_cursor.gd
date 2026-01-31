21a6522fe308278e5d7bee10ec7322a9a78c2a11
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2026-01-31 17:29:33 +0100

    Implement BitmaskerCursor

---
+++ bitmaskers/cursor/bitmask_cursor.gd
@@ -0,0 +1,61 @@
+class_name BitmaskCursor
+extends Sprite2D
+
+
+const POSITION_EASING: float = 10
+const DEAD_ZONE: float = 0.35
+const MOVEMENT_INTERVAL: float = 0.2
+
+
+var bitmasker: Bitmasker
+var tile_position: Vector2i
+var target_position: Vector2
+var can_move: bool = true
+
+
+func _process(delta: float) -> void:
+	self.global_position.x = move_toward(
+		self.global_position.x, 
+		self.target_position.x, 
+		absf(self.global_position.x - self.target_position.x) * POSITION_EASING * delta
+	)
+	self.global_position.y = move_toward(
+		self.global_position.y, 
+		self.target_position.y, 
+		absf(self.global_position.y - self.target_position.y) * POSITION_EASING * delta
+	)
+	
+	if can_move:
+		var joy_x: float = Input.get_joy_axis(bitmasker.player.controller_id, JOY_AXIS_RIGHT_X)
+		var joy_y: float = Input.get_joy_axis(bitmasker.player.controller_id, JOY_AXIS_RIGHT_Y)
+		
+		var movement_vector: Vector2i
+		
+		if joy_x > DEAD_ZONE:
+			movement_vector.x = -1
+		elif joy_x < -DEAD_ZONE:
+			movement_vector.x = 1
+			
+		if joy_y > DEAD_ZONE:
+			movement_vector.y = -1
+		elif joy_y < -DEAD_ZONE:
+			movement_vector.y = 1
+			
+		set_tile_position(tile_position + movement_vector)
+		
+		if movement_vector != Vector2i.ZERO:
+			can_move = false
+			get_tree().create_timer(MOVEMENT_INTERVAL).timeout.connect(
+				func() -> void:
+					can_move = true
+			)
+
+
+func set_tile_position(new_pos: Vector2i) -> void:
+	tile_position = new_pos
+	self.target_position = bitmasker.tile_to_global(new_pos)
+	
+	
+func set_tile_position_instant(new_pos: Vector2i) -> void:
+	set_tile_position(new_pos)
+	self.global_position = bitmasker.tile_to_global(new_pos)
