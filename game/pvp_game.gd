bf3bf161f147f3884206725986a43529a95884cf
Enderite <85014963+EnderiteFox@users.noreply.github.com>
2026-01-31 11:52:48 +0100

    Implement map spawning

--- game/pvp_game.gd
+++ game/pvp_game.gd
@@ -2,10 +2,26 @@ class_name PvpGame
 extends GameInstance
 
 
+const maps: Array[PackedScene] = [
+	preload("uid://dby6dfhhwlgc"),
+	preload("uid://cta62jwvbhmq4")
+]
+
+
+var map: Map
+
+
 func start_game() -> void:
+	var map_scene: PackedScene = maps.pick_random()
+	map = map_scene.instantiate()
+	self.add_child(map)
+	
 	for player: PlayerData in Game.players:
 		var player_ship: PlayerShip = ship_scene.instantiate()
 		player_ship.set_player(player)
 		player.ship = player_ship
 		self.add_child(player_ship)
-		player_ship.global_position = Vector2.ZERO
+		
+		var spawn_point: Vector2 = map.spawn_points.pick_random()
+		map.spawn_points.erase(spawn_point)
+		player_ship.global_position = map.to_global(spawn_point)
