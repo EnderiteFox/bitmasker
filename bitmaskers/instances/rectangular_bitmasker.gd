9c3af141144e34ec4b5bd8a07be35e4e4c58fabb
NotARobot3 <parineymaxence@gmail.com>
2026-03-10 18:54:51 +0100

    fixed ship hit speed boost bug

--- bitmaskers/instances/rectangular_bitmasker.gd
+++ bitmaskers/instances/rectangular_bitmasker.gd
@@ -4,8 +4,8 @@ extends Bitmasker
 
 
 const MAX_SIZE: int = 60
-const MOVEMENT_INTERVAL_CONFIRM: float = -0.05
-const MOVEMENT_INTERVAL_SELECT: float = -0.025
+const MOVEMENT_INTERVAL_CONFIRM: float = BitmaskCursor.DEFAULT_MOVEMENT_INTERVAL - 0.05
+const MOVEMENT_INTERVAL_SELECT: float = BitmaskCursor.DEFAULT_MOVEMENT_INTERVAL - 0.025
 
 
 
@@ -35,12 +35,11 @@ func on_select() -> void:
 	cursor = self.spawn_cursor()
 	cursor.moved.connect(_on_cursor_move)
 	clear_selection()
-	cursor.movement_interval = MOVEMENT_INTERVAL_SELECT + BitmaskCursor.DEFAULT_MOVEMENT_INTERVAL
+	cursor.movement_interval = MOVEMENT_INTERVAL_SELECT
 
 
 func on_unselect() -> void:
 	if cursor != null and not selection_complete:
-		cursor.movement_interval = BitmaskCursor.DEFAULT_MOVEMENT_INTERVAL
 		cursor.queue_free()
 		cursor = null
 		selection_start = Vector2i.MAX
@@ -51,7 +50,7 @@ func on_confirm() -> void:
 	if cursor == null:
 		return
 	
-	cursor.movement_interval = BitmaskCursor.DEFAULT_MOVEMENT_INTERVAL + MOVEMENT_INTERVAL_CONFIRM
+	cursor.movement_interval = MOVEMENT_INTERVAL_CONFIRM
 	
 	if selection_start == Vector2i.MAX:
 		selection_start = Vector2i(cursor.tile_position)
@@ -97,7 +96,7 @@ func get_ship_target_pos() -> Vector2:
 	if cursor == null:
 		return Vector2.INF
 		
-	return cursor.global_position - tile_to_global(Vector2i(1, 1)) / 2
+	return cursor.global_position + tile_to_global(Vector2i(1, 1)) / 2
 
 
 ## Called when the cursor moves
